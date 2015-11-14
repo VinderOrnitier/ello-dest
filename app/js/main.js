1fd55119cca3b7d5f00079d5d460051d240c489e
Vinder Ornitier <ornitier62@gmail.com>
2015-11-14 21:16:57 +0200

    main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+	// Animated Hamburger Menu
+	$('.hamburger-menu').on('click', function() {
+		$('.bar').toggleClass('animate-bar');
+		$('.menu-nav').slideToggle();
+		return false;
+	});
+
+	$('.footer-hamburger-menu').on('click', function() {
+		$('.footer-bar').toggleClass('footer-animate-bar');
+		$('.footer-nav').slideToggle();
+		return false;
+	});
+
+	// Слайдер http://www.owlcarousel.owlgraphic.com/
+	$(".owl-carousel").owlCarousel({
+		items:1,
+		loop:true,
+		nav:true,
+		autoHeight:true,
+		autoplay:true,
+		autoplayHoverPause:true,
+		navText:false,
+		lazyLoad:true,
+		smartSpeed:450
+	});
+
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
